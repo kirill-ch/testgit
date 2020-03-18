4351538a5f85a98daf9a82a36463e55516228684
Kirill Chokparov <kirill.m.ch@gmail.com>
2020-03-18 16:54:04 +0300

    clear

--- src/main/java/ru/kirillch/App.java
+++ src/main/java/ru/kirillch/App.java
@@ -2,8 +2,6 @@ package ru.kirillch;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("12345");
-        System.out.println("ASWQERT");
-        System.out.println("asdcv");
+
     }
 }
