d0e1ee69ecc2edef36c6b8790a1ef4d76ed39316
Kirill Chokparov <kirill.m.ch@gmail.com>
2020-03-18 16:46:42 +0300

    clear

--- src/main/java/ru/kirillch/App.java
+++ src/main/java/ru/kirillch/App.java
@@ -2,9 +2,6 @@ package ru.kirillch;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("12345");
-        System.out.println("abc");
-        System.out.println("QWERTY");
-        System.out.println("QWERTY");
+
     }
 }
