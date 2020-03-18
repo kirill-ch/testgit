f4452d40d4d0f9631adfbc4a4aae5300f1c0ef6f
Kirill Chokparov <kirill.m.ch@gmail.com>
2020-03-18 16:44:25 +0300

    adds

--- src/main/java/ru/kirillch/App.java
+++ src/main/java/ru/kirillch/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("12345");
         System.out.println("QWERTY");
+        System.out.println("QWERTY");
     }
 }
