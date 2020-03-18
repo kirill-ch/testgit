5c8bbb8dc1a5d9ad07c7ace04d788d470a725122
Kirill Chokparov <kirill.m.ch@gmail.com>
2020-03-18 16:43:57 +0300

    adds

--- src/main/java/ru/kirillch/App.java
+++ src/main/java/ru/kirillch/App.java
@@ -3,6 +3,7 @@ package ru.kirillch;
 public class App {
     public static void main(String[] args) {
         System.out.println("12345");
+        System.out.println("abc");
         System.out.println("QWERTY");
     }
 }
