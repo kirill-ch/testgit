0a68c34a00c82fa9b17e0a22534839f1429de9eb
Kirill Chokparov <kirill.m.ch@gmail.com>
2020-03-18 17:30:57 +0300

    clear

--- src/main/java/ru/kirillch/App.java
+++ src/main/java/ru/kirillch/App.java
@@ -2,10 +2,6 @@ package ru.kirillch;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("12345");
-        System.out.println("AQWQWQWQWQWQW");
-        System.out.println("фывафваAS");
-        System.out.println("фывафва123412341");
-        System.out.println("фывафваASASASS123132asdfasdfasf");
+
     }
 }
