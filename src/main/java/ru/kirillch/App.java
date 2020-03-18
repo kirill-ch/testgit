66d5ecde14e556ff6e9f8512820e2c51e4e788fa
Kirill Chokparov <kirill.m.ch@gmail.com>
2020-03-18 17:11:44 +0300

    adds

--- src/main/java/ru/kirillch/App.java
+++ src/main/java/ru/kirillch/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("12345");
         System.out.println("AQWQWQWQWQWQW");
+        System.out.println("фывафва123412341");
         System.out.println("фывафваASASASS");
     }
 }
